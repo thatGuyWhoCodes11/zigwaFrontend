2d19b98318da70739e9d8b5d3f82d8ba067b19d3
ahmadharthey <ahmoood@outlook.com>
2023-08-07 19:59:45 +0300

    not much been added, just reasearch about nested navs, kms

--- App.js
+++ App.js
@@ -2,19 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Sign_Up from './screens/Sign_Up';
 import Sign_In from './screens/Sign_In';
 import Citizen from './screens/Citizen';
-
+import Map from './screens/Map'
 const stack = createStackNavigator()
+const drawer = createDrawerNavigator()
+
+function DrawerCitizen(){
+  return(
+    <drawer.Navigator>
+      <drawer.Screen name='home' component={Citizen} />
+      <drawer.Screen name='map' component={Map} />
+    </drawer.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <stack.Navigator >
-        <stack.Screen name='Sign_Up' component={Sign_Up} options={{title:"", headerTransparent:true}} />
-        <stack.Screen name='Citizen' component={Citizen} />
-        <stack.Screen name='Sign_In' component={Sign_In} options={{title:"", headerTransparent:true}} />
+      <stack.Navigator screenOptions={{headerShown:false}} >
+        <stack.Screen name='Sign_Up' component={Sign_Up} options={{ title: "", headerTransparent: true }} />
+        <stack.Screen name='Sign_In' component={Sign_In} options={{ title: "", headerTransparent: true }} />
+        <stack.Screen name='Drawer' component={DrawerCitizen}  />
       </stack.Navigator>
     </NavigationContainer>
   );
@@ -26,3 +37,5 @@ const styles = StyleSheet.create({
   }
 
 });
+//TO DO make sure to make a seperate folder for each role inside of screens, due to how navigations work, you will have to make nested ones, stackNav that leads to drawerNav
+//each role will have their own drawer navigator, i hope i don't forget this
--- screens/Citizen.js
+++ screens/Citizen.js
@@ -1,32 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,View, Modal, Button } from 'react-native';
+import { StyleSheet, View, Modal, Button } from 'react-native';
 import { useState } from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 export default function Citizen({ route, navigation }) {
-[mapModal,setMapModal]=useState(false)
+  [mapModal, setMapModal] = useState(false)
   return (
-    <View>
-      <View style={{alignSelf:'center'}}>
-        <Button title='show map' onPress={()=>{setMapModal(true)}}/>
-      </View>
-      {mapModal && <Modal>
-      <MapView
-      style={styles.map}
-        initialRegion={{
-          latitude: 0,
-          longitude: 0,
-          latitudeDelta: 0,
-          longitudeDelta: 0,
-        }}
-      />
-      </Modal>}
-    </View>
+    <Text>placeholder</Text>
   )
 }
 
 const styles = StyleSheet.create({
-  map:{
-    height:'100%',
-    width:'100%'
+  map: {
+    height: '100%',
+    width: '100%'
   }
 });
